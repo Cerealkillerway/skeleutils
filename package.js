1f0afb926598cc1c342773ea4000809c8806bf6e
cerealkillerway <cerealkillerway@gmail.com>
2015-11-21 06:15:13 +0100

    initial commit

---
+++ package.js
@@ -0,0 +1,80 @@
+Package.describe({
+    name: 'cerealkiller:utils',
+    version: '1.0.0',
+    // Brief, one-line summary of the package.
+    summary: 'utilities and common functions',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.1');
+
+    // packages
+
+    //exports
+    api.export('ckUtils');   // package namespace
+
+    // styles
+
+    // templates
+    
+    // libraries
+    api.addFiles('namespace.js');
+    api.addFiles('lib/commonUtilities.js', 'client');
+
+    // i18n
+
+});
+
+Package.onTest(function(api) {
+    api.use('tinytest');
+    api.use('cerealkiller:ckutils');
+    api.addFiles('ckutils-tests.js');
+});
+
+
+
+
+// UTILITIES
+// get list of all files in a folder
+function getFilesFromFolder(packageName, folder){
+    // local imports
+    var _ = Npm.require("underscore");
+    var fs = Npm.require("fs");
+    var path = Npm.require("path");
+    // helper function, walks recursively inside nested folders and return absolute filenames
+    function walk(folder){
+        var filenames = [];
+        // get relative filenames from folder
+        var folderContent=fs.readdirSync(folder);
+        // iterate over the folder content to handle nested folders
+        _.each(folderContent,function(filename){
+            // build absolute filename
+            var absoluteFilename=folder + path.sep + filename;
+            // get file stats
+            var stat=fs.statSync(absoluteFilename);
+            if (stat.isDirectory()){
+                // directory case => add filenames fetched from recursive call
+                filenames=filenames.concat(walk(absoluteFilename));
+            }
+            else {
+                // file case => simply add it
+                filenames.push(absoluteFilename);
+            }
+        });
+        return filenames;
+    }
+    // save current working directory (something like "/home/user/projects/my-project")
+    var cwd = process.cwd();
+    // chdir to our package directory
+    process.chdir("packages" + path.sep + packageName);
+    // launch initial walk
+    var result = walk(folder);
+    // restore previous cwd
+    process.chdir(cwd);
+    return result;
+}
